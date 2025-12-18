7ee97bcb80f6293e5a32c83ebc54e67100462aaa
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-12-18 00:10:08 -0600

    song

--- Assets/2-Jolly Ranchers/script.cs
+++ Assets/2-Jolly Ranchers/script.cs
@@ -7,11 +7,11 @@ using TMPro;
 
 namespace JollyRanchers{
 	public class script : MonoBehaviour {
+		public AnimationCurve SpacePressesNeeded;
 		public TextMeshProUGUI DifficultyText;
 		public TextMeshProUGUI UIText;
-		public AnimationCurve SpacePressesNeeded;
-		public Image player;
 		public string winText;
+		public Image player;
 
 		public AudioClip loopSound;
 		public AudioClip winSound;
@@ -19,21 +19,26 @@ namespace JollyRanchers{
 		private int spaceCount;
 
 		private bool isWin = false;
+
 		private World wrld;
 		private WorldObject plr;
 
 		private float minX = 160.0f;
 		private float maxX = 815.0f;
-		private float dX = 1.0f;
+		private float dX = 300.0f;
 
 		private float minY = 119.0f;
 		private float maxY = 420.0f;
-		private float dY = 1.0f;
+		private float dY = 300.0f;
 
 		private void Start(){
+			isWin = false;
+			
+			// Difficulty
 			float difficulty = Managers.MinigamesManager.GetCurrentMinigameDifficulty();
 			DifficultyText.text = $"Current Difficulty: {difficulty.ToString()}";
 			spaceCount = Mathf.CeilToInt(SpacePressesNeeded.Evaluate(difficulty));
+			// ------------------------------------------------------------------ //
 
 			UIText.text = $"Press space {spaceCount} times!";
 			UIText.text = $"Press space {spaceCount} times! | {UIText.transform.position}";
@@ -41,25 +46,34 @@ namespace JollyRanchers{
 			
 			UIText.transform.position = new Vector3(490,334,0);
 			
+			// Play Looped song
 			AudioSource loop = Managers.AudioManager.CreateAudioSource();
 			loop.loop = true;
 			loop.clip = loopSound;
 			loop.Play();
+			// --------- //
 
+			// Set up game World
 			wrld = new World(maxX,maxY,minX,minY);
+
 			plr = new WorldObject(wrld.Min_X,wrld.Min_Y,50.0f,50.0f);
 			player.transform.position = new Vector3(plr.x,plr.y,0);
+
+			//GameObject newImageObject = new GameObject("Image");
+			//newImageObject.transform.SetParent([Replace with canvas].transform, false);
+			//Image newImage = newImageObject.AddComponent<Image>();
 		}
 
 		private void Update(){
-			float x = dX * Input.GetAxis("Horizontal");
+			// move player around
+			float x = Time.deltaTime * dX * Input.GetAxis("Horizontal");
 			if(wrld.collideWorldBorder_X(plr.x + x)){
 				x = 0.0f;
 			}
 			plr.x = plr.x + x;
 
 			/*
-			float y = dY * Input.GetAxis("Vertical");
+			float y = Time.deltaTime * dY * Input.GetAxis("Vertical");
 			if(wrld.collideWorldBorder_Y(plr.y + y)){
 				y = 0.0f;
 			}
@@ -67,6 +81,7 @@ namespace JollyRanchers{
 			*/
 
 			player.transform.position = new Vector3(plr.x,plr.y,0);
+			// --------------------------------------------------- //
 			UIText.text = $"{player.transform.position}";
 
 			if (Input.GetButtonDown("Space")){
@@ -76,8 +91,10 @@ namespace JollyRanchers{
 			}
 
 			if(spaceCount == 0){
+				isWin = true;
+			}
+			if(isWin){
 				UIText.text = winText;
-
 				AudioSource win = Managers.AudioManager.CreateAudioSource();
 				win.PlayOneShot(winSound);
 
