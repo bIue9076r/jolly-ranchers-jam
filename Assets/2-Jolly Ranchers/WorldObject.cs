8392f9904b5b0ca8871ec6e99f1d70db43a82638
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2025-12-18 12:47:10 -0600

    skeleton game

--- Assets/2-Jolly Ranchers/WorldObject.cs
+++ Assets/2-Jolly Ranchers/WorldObject.cs
@@ -1,6 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 namespace JollyRanchers{
 	public class WorldObject{
 		public float x, y, w, h;
+		public Image img;
+		public bool doCollide = true;
+
+		public WorldObject(float x, float y, float w, float h, Image img){
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+
+			this.img = img;
+			this.img.rectTransform.sizeDelta = new Vector2(this.w, this.h);
+		}
 
 		public WorldObject(float x, float y, float w, float h){
 			this.x = x;
@@ -11,18 +27,22 @@ namespace JollyRanchers{
 
 		public void setX(float x){
 			this.x = x;
+			return;
 		}
 
 		public void setY(float y){
 			this.y = y;
+			return;
 		}
 
 		public void setWidth(float w){
 			this.w = w;
+			return;
 		}
 
 		public void setHeight(float h){
 			this.h = h;
+			return;
 		}
 
 		public float getX(){
@@ -47,6 +67,19 @@ namespace JollyRanchers{
 			}
 
 			return false;
+		}
+
+		public bool collide(WorldObject obj){
+			if(((this.x + this.w) >= obj.x) && (this.x <= (obj.x + obj.w)) && ((this.y + this.h) >= obj.y) && (this.y <= (obj.y + obj.h))){
+				return true;
+			}
+
+			return false;
+		}
+
+		public void move(){
+			this.img.transform.position = new Vector3(this.x,this.y,0);
+			return;
 		}
 	}
 }
--- Assets/2-Jolly Ranchers/script.cs
+++ Assets/2-Jolly Ranchers/script.cs
@@ -7,21 +7,25 @@ using TMPro;
 
 namespace JollyRanchers{
 	public class script : MonoBehaviour {
-		public AnimationCurve SpacePressesNeeded;
-		public TextMeshProUGUI DifficultyText;
 		public TextMeshProUGUI UIText;
+		public Canvas parent;
 		public string winText;
-		public Image player;
 
 		public AudioClip loopSound;
 		public AudioClip winSound;
+		public AudioClip lossSound;
+		//public AudioClip collectSound;
 
 		private int spaceCount;
 
 		private bool isWin = false;
+		private bool isLoss = false;
 
 		private World wrld;
 		private WorldObject plr;
+		private List<WorldObject> snow;
+		private int snowCount;
+		private int caught = 0;
 
 		private float minX = 160.0f;
 		private float maxX = 815.0f;
@@ -29,22 +33,15 @@ namespace JollyRanchers{
 
 		private float minY = 119.0f;
 		private float maxY = 420.0f;
-		private float dY = 300.0f;
+		private float dY = 140.0f;
 
-		private void Start(){
-			isWin = false;
-			
+		private void Start(){			
 			// Difficulty
 			float difficulty = Managers.MinigamesManager.GetCurrentMinigameDifficulty();
-			DifficultyText.text = $"Current Difficulty: {difficulty.ToString()}";
-			spaceCount = Mathf.CeilToInt(SpacePressesNeeded.Evaluate(difficulty));
+			snowCount = Mathf.CeilToInt(3.0f*difficulty + 2.0f);
 			// ------------------------------------------------------------------ //
 
-			UIText.text = $"Press space {spaceCount} times!";
-			UIText.text = $"Press space {spaceCount} times! | {UIText.transform.position}";
-			UIText.text = $"{player.transform.position}";
-			
-			UIText.transform.position = new Vector3(490,334,0);
+			UIText.text = $"Catch {snowCount} snow!";
 			
 			// Play Looped song
 			AudioSource loop = Managers.AudioManager.CreateAudioSource();
@@ -56,12 +53,25 @@ namespace JollyRanchers{
 			// Set up game World
 			wrld = new World(maxX,maxY,minX,minY);
 
-			plr = new WorldObject(wrld.Min_X,wrld.Min_Y,50.0f,50.0f);
-			player.transform.position = new Vector3(plr.x,plr.y,0);
+			snow = new List<WorldObject>();
+			for(int i = 0; i < snowCount; i++){
+				GameObject newImageObject = new GameObject("Snow");
+				newImageObject.transform.SetParent(parent.transform, false);
+				Image newImage = newImageObject.AddComponent<Image>();
+				newImage.color = new Color(0.5f,0.5f,0.5f,1.0f);
+				snow.Add(new WorldObject(
+					(524.0f * UnityEngine.Random.Range(0.0f,1.0f)) + 225.5f,
+					maxY + ((800.0f * i)/snowCount),
+					50.0f,50.0f,newImage));
+				snow[i].move();
+			}
 
-			//GameObject newImageObject = new GameObject("Image");
-			//newImageObject.transform.SetParent([Replace with canvas].transform, false);
-			//Image newImage = newImageObject.AddComponent<Image>();
+			GameObject newPlrImageObject = new GameObject("Snow");
+			newPlrImageObject.transform.SetParent(parent.transform, false);
+			Image newPlrImage = newPlrImageObject.AddComponent<Image>();
+			newPlrImage.color = new Color(1.0f,0.0f,0.0f,1.0f);
+			plr = new WorldObject(minX,minY,50.0f,50.0f,newPlrImage);
+			plr.move();
 		}
 
 		private void Update(){
@@ -71,34 +81,52 @@ namespace JollyRanchers{
 				x = 0.0f;
 			}
 			plr.x = plr.x + x;
+			plr.move();
 
-			/*
-			float y = Time.deltaTime * dY * Input.GetAxis("Vertical");
-			if(wrld.collideWorldBorder_Y(plr.y + y)){
-				y = 0.0f;
+			// Move snow around
+			foreach(WorldObject obj in snow){
+				if(obj.doCollide){
+					float y = Time.deltaTime * (-dY);
+					obj.y = obj.y + y;
+					obj.move();
+
+					if(obj.collide(plr)){
+						//AudioSource collect = Managers.AudioManager.CreateAudioSource();
+						//collect.PlayOneShot(collectSound);
+						obj.img.enabled = false;
+						obj.doCollide = false;
+						caught++;
+					}
+
+					if(obj.y <= minY - obj.h){
+						isLoss = true;
+					}
+				}
 			}
-			plr.y = plr.y + y;
-			*/
-
-			player.transform.position = new Vector3(plr.x,plr.y,0);
 			// --------------------------------------------------- //
-			UIText.text = $"{player.transform.position}";
 
-			if (Input.GetButtonDown("Space")){
-				spaceCount--;
-				UIText.text = $"Press space {spaceCount} times!";
-				UIText.text = $"Press space {spaceCount} times! | {UIText.transform.position}";
-			}
+			UIText.text = $"Catch {snowCount} snow!";
 
-			if(spaceCount == 0){
+			if(caught >= snowCount){
 				isWin = true;
 			}
+
 			if(isWin){
 				UIText.text = winText;
 				AudioSource win = Managers.AudioManager.CreateAudioSource();
 				win.PlayOneShot(winSound);
 
 				Managers.MinigamesManager.DeclareCurrentMinigameWon();
+				Managers.MinigamesManager.EndCurrentMinigame(1f);
+				this.enabled = false;
+			}
+
+			if(isLoss){
+				UIText.text = "Snow dropped!";
+				AudioSource loss = Managers.AudioManager.CreateAudioSource();
+				loss.PlayOneShot(lossSound);
+
+				Managers.MinigamesManager.DeclareCurrentMinigameLost();
 				Managers.MinigamesManager.EndCurrentMinigame(1f);
 				this.enabled = false;
 			}
